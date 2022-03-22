17663654d1e6cfd024975258dc69d5295e21c1fd
Kenroy 'wiz' Gayle <wiz_kidz@outlook.com>
2022-03-21 21:59:21 -0500

    Initial commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by wizz on 11/5/21.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Restart/Screens/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by wizz on 11/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    var body: some View {
+        ZStack{
+            if isOnboardingViewActive{
+                OnboardingView()
+            }else{
+                HomeView()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
